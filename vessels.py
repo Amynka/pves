e24928a52f0885762e6e5a5792e71d4689d9091d
Amy Liffey <amynka@gentoo.org>
2019-08-12 21:20:23 +0200

    Add read_images function

--- vessels.py
+++ vessels.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python3
 
 import argparse
-import numpy
+import sys
+import tqdm
 import javabridge
+import numpy as np
 import bioformats as bf
-from PIL import Image
 
-
-# Print shape of the array
-#print('Shape of numpy array: {}'.format(nim.shape))
-
-# Print size
-#print('Size of numpy: {}'.format(nim.size))
-
-# Print the array
-#print('Numpy array: {}'.format(nim))
 
 # Read metadata and return metadata as object
 def get_metadata(filename):
     metadata = bf.get_omexml_metadata(filename)
     return bf.omexml.OMEXML(metadata)
+
+
+# Read the image
+def read_images(filename):
+    with bf.ImageReader(filename) as reader:
+        # Shape of the data
+        c_total = reader.rdr.getSizeC()
+        x_total = reader.rdr.getSizeX()
+        y_total = reader.rdr.getSizeY()
+        z_total = reader.rdr.getSizeZ()
+        t_total = reader.rdr.getSizeT()
+        print('Number of color planes: {}'.format(c_total))
+        print('Image Resolution: {}x{}'.format(x_total, y_total))
+        print('Image depth: {}'.format(z_total))
+        print('Number of frames in the image: {}'.format(t_total))
+
+        # Hyperstacks not suspported yet
+        if 1 not in [z_total, t_total]:
+            raise TypeError('Only 4D images are currently supported.')
+
+        #metadata = get_metadata(filename)
+
+        images = []
+        for time in tqdm.tqdm(range(t_total), "T"):
+            # Read image
+            image = reader.read(t=time, rescale=False)
+            images.append(image)
+    return np.asarray(images)
 
 
 def main() -> int:
@@ -29,14 +49,16 @@ def main() -> int:
     args = parser.parse_args()
 
     # Start javabridge vm
-    javabridge.start_vm(class_path=bf.JARS, max_heap_size='2G')
+    javabridge.start_vm(class_path=bf.JARS, max_heap_size='8G')
 
     # Print metadata
-    print(get_metadata(args.file))
+    m = get_metadata(args.file)
+    print(m)
+    print(read_images(args.file))
 
     # Kill the javabridge vm
     javabridge.kill_vm()
 
 
 if __name__ == '__main__':
-    exit(main())
+    sys.exit(main())
