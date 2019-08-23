0ea46d18afead89127fc59c1d6a6a2b0d2a9199b
Amy Liffey <amynka@gentoo.org>
2019-08-23 21:52:00 +0200

    Add loading of images in chunks

--- vessels.py
+++ vessels.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import sys
 import tqdm
 import xml.dom.minidom
 import javabridge
+import fnmatch
 import numpy as np
 import bioformats as bf
 import matplotlib.widgets as widgets
@@ -25,6 +27,15 @@ def save_metadata(metadata, filename):
         xmlfile.write(pretty_xml_as_string)
 
 
+# Read first image
+def read_first(filename):
+    with bf.ImageReader(filename) as reader:
+        image = reader.read(t=0, rescale=False)
+        #a, b = np.split(image, 2, axis=1)
+    #return np.asarray(a), np.asarray(b)
+    return np.asarray(image)
+
+
 # Read the image
 def read_images(filename):
     with bf.ImageReader(filename) as reader:
@@ -43,14 +54,26 @@ def read_images(filename):
         if 1 not in [z_total, t_total]:
             raise TypeError('Only 4D images are currently supported.')
 
-        right, left = [], []
-        for time in tqdm.tqdm(range(t_total), "Loading frames"):
-            # Read image
-            image = reader.read(t=time, rescale=False)
-            a, b = np.split(image, 2, axis=1)
-            left.append(a)
-            right.append(b)
-    return np.asarray(right), np.asarray(left)
+        counter = t_total//10000
+        file = filename.split('/')[1]
+        # right, left = [], []
+        frame = 0
+        count = 1
+        total = (t_total//counter)
+        # TO DO redo the loading to not lose any frames
+        while count <= counter:
+            right, left = [], []
+            for time in tqdm.tqdm(range((count-1)*frame, count*total), "Loading frames"):
+                # Read image
+                image = reader.read(t=time, rescale=False)
+                a, b = np.split(image, 2, axis=1)
+                left.append(a)
+                right.append(b)
+            np.save(str(count) + '-' + file + '-left.npy', np.asarray(left))
+            np.save(str(count) + '-' + file + '-right.npy', np.asarray(right))
+            frame = (t_total//counter)
+            count += 1
+        print('Images loaded')
 
 
 def crop(array, x1, x2, y1, y2):
@@ -94,19 +117,44 @@ def main() -> int:
     args = parser.parse_args()
 
     if args.load:
+        # TO DO parametrize the load for more files ======
+
+        # print(args.file.split('/')[0])
+        # print(fnmatch.filter(os.listdir('.'), '*' + args.file.split('/')[1] + '-left.npy'))
+        # print(fnmatch.filter(os.listdir('.'), '*' + args.file.split('/')[1] + '-right.npy'))
+        # javabridge.start_vm(class_path=bf.JARS, max_heap_size='8G')
+        # r = read_first(args.file)
+        # javabridge.kill_vm()
+        # # Select region in the function
+        # from matplotlib import pyplot as plt
+        # print("\n      click  -->  release")
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111)
+        # ax.imshow(r)
+        # # drawtype is 'box' or 'line' or 'none'
+        # toggle_selector.RS = widgets.RectangleSelector(ax, line_select_callback,
+        #                                drawtype='box', useblit=True,
+        #                                button=[1, 3],  # don't use middle button
+        #                                minspanx=5, minspany=5,
+        #                                spancoords='pixels',
+        #                                interactive=True)
+        # plt.connect('key_press_event', toggle_selector)
+        # plt.show()
+
         right = np.load(args.file + '-right.npy')
         left = np.load(args.file + '-left.npy')
+        #return 0
     else:
         javabridge.start_vm(class_path=bf.JARS, max_heap_size='8G')
         # Get and save metadata
         m = get_metadata(args.file)
         save_metadata(m, args.file)
-        right, left = read_images(args.file)
-        np.save(args.file + '-right.npy', right)
-        np.save(args.file + '-left.npy', left)
-        print(right.shape, left.shape)
+        # Read and save images as npy
+        read_images(args.file)
         # Kill javabridge vm
         javabridge.kill_vm()
+        print('Java VM killed')
+        return 0
 
     from matplotlib import pyplot as plt
     fig = plt.figure()
@@ -134,7 +182,7 @@ def main() -> int:
     plt.connect('key_press_event', toggle_selector)
     plt.show()
 
-    # Crop according to selected region
+    # Crop according to selected region and average region
     lcrop, rcrop = [], []
     l_avg, r_avg = [], []
     for i in range(0, len(left)-1):
@@ -147,14 +195,17 @@ def main() -> int:
     np_subtr = np.subtract(lcrop[12], rcrop[12])
     np_subtr2 = np.subtract(rcrop[12], lcrop[12])
     # Show random cropped image
-    print(lcrop.shape, rcrop.shape)
     fig = plt.figure()
     ax1 = fig.add_subplot(3, 1, 1)
     ax1.imshow(np.concatenate((lcrop[12], rcrop[12], np_subtr, np_subtr2), axis=1))
+
+    # Plot the averages into the graphs
     ax2 = fig.add_subplot(3, 1, 2)
     ax2.plot(l_avg, label='Astrocytes')
-    ax2.plot(r_avg, label='Neurons')
     ax2.legend()
+    ax3 = fig.add_subplot(3, 1, 3)
+    ax3.plot(r_avg, label='Neurons')
+    ax3.legend()
     plt.show()
 
 
